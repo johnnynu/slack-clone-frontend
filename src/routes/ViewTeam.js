8e3569edec7855558acc7235563bddd13309ef39
johnnynu <johnnynu98@gmail.com>
2021-08-27 03:36:38 -0700

    added props to components

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -4,22 +4,38 @@ import AppLayout from '../components/AppLayout';
 import Channels from '../components/Channels';
 import Teams from '../components/Teams';
 import Messages from '../components/Messages';
-import Input from '../components/Input';
+import SendMessage from '../components/SendMessage';
 import Header from '../components/Header';
 
 export default () => (
   <AppLayout>
-    <Teams>Teams</Teams>
-    <Channels>Channels</Channels>
-    <Header className="header">Header</Header>
+    <Teams
+      teams={[
+        { id: 1, letter: 'C' },
+        { id: 2, letter: 'S' },
+      ]}
+    />
+    <Header channelName="general" />
+    <Channels
+      teamName="Team Name"
+      username="Username"
+      channels={[
+        { id: 1, name: 'general' },
+        { id: 2, name: 'random' },
+      ]}
+      users={[
+        { id: 1, name: 'slackbot' },
+        { id: 2, name: 'nonuser' },
+      ]}
+    />
     <Messages className="messages">
       <ul className="message-list">
         <li />
         <li />
       </ul>
     </Messages>
-    <Input>
+    <SendMessage channelName="general">
       <input type="text" placeholder="send a message!" />
-    </Input>
+    </SendMessage>
   </AppLayout>
 );
